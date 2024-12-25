aa02426de2f9f20515a29b122f81477222385761
hafiiddd <hafid.akhyar0411@gmail.com>
2024-12-25 15:41:40 +0700

    add expertise,admin,article API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import db from "./db.js";
 import express from "express";
+import cors from 'cors';
 import VerifySanctumToken from "./Middleware/verifySanctumToken.js";
 import { getAllCounselor } from "./Counselor/counselor.service.js";
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -10,6 +12,10 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+app.use(cors({
+  origin: "http://localhost:5173",  
+  allowedHeaders: ['Content-Type', 'Authorization']  
+}));
 
 app.get("/test", VerifySanctumToken, async (req, res) => {
   try {
@@ -24,5 +30,11 @@ app.get("/test", VerifySanctumToken, async (req, res) => {
 });
 
 import productController from "./Counselor/counselor.controller.js";
+import expertiseController from "./Expertise/expertise.controller.js";
+import articleController from "./Article/article.controller.js";
+import adminController from "./admin/admin.controller.js";
 
 app.use("/", VerifySanctumToken, productController);
+app.use("/", VerifySanctumToken, expertiseController);
+app.use("/", VerifySanctumToken, articleController);
+app.use("/", VerifySanctumToken, adminController);
